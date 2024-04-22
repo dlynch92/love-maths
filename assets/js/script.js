4742803e54b10c2174b903ec6d05ec1e9d0a380d
dlynch92 <daniellynch92@gmail.com>
2024-04-22 18:24:57 +0100

    add function to calulate the correct answer for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,9 +40,21 @@ function runGame(gameType){
 function checkAnswer(){
     
 }
-
+/**
+ * Gets the operands and the operator directly from the dom & returns the
+ * correct answer
+ */
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert("Unimplemented operator");
+        throw "Unimplemented operator. Aborting.";
+    }
 }
 
 function incrementScore(){
