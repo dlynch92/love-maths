a744a57caf4b7103fe19a99716a2857adce547eb
dlynch92 <daniellynch92@gmail.com>
2024-04-24 18:44:48 +0100

    add final tidy ups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 });
 
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * user's answer has been processed.
  */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //Generates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -59,6 +68,9 @@ function checkAnswer(){
         alert(`You said: ${userAnswer}. The correct answer was: ${calculatedAnswer[0]}. Booo.`);
         incrementWrongAnswer();
     }
+
+    runGame(calculatedAnswer[1]);
+
 }
 
 /**
